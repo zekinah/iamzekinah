af00b011fe917ce69989304b5deca205479590bf
zekinah <zjlecaros@gmail.com>
2020-06-21 16:54:31 -0700

    Removes dev/sw.js

--- vue.config.js
+++ vue.config.js
@@ -6,11 +6,6 @@ module.exports = {
       themeColor: '#81ABAE',
       msTileColor: '#223140',
       appleMobileWebAppCapable: 'yes',
-      appleMobileWebAppStatusBarStyle: 'black',
-      // configure the workbox plugin
-      workboxPluginMode: 'InjectManifest',
-      workboxOptions: {
-        swSrc: 'dev/sw.js',
-      }
+      appleMobileWebAppStatusBarStyle: 'black'
     }
   }
