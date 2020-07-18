ba9a7f39ce65d36ccd4e0c7d95d33b9c31e12734
zekinah <zjlecaros@gmail.com>
2020-07-18 16:01:43 -0700

    Updated the About - Otherstuffs

--- src/components/SectionOtherstuff.vue
+++ src/components/SectionOtherstuff.vue
@@ -2,7 +2,18 @@
   <div id="__otherstuff" class="columns is-desktop section">
     <div class="column section__title is-one-fifth">Other Stuffs</div>
     <div class="column section__content">
-      <codersrank-widget username="zekinah"></codersrank-widget>
+      <div class="otherstuff__list">
+        <div class="otherstuff__name">Coders Rank Stats</div>
+        <div class="otherstuff__content">
+          <codersrank-widget username="zekinah"></codersrank-widget>
+        </div>
+      </div>
+      <div class="otherstuff__list">
+        <div class="otherstuff__name">Github Stats</div>
+        <div class="otherstuff__content">
+            <p><a href="https://github.com/zekinah" target="_blank"><img :src="githubStatsUrl" alt="zekinah-stats"></a></p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,7 +26,8 @@ export default {
           { id: 1, name: "Badminton"},
           { id: 2, name: "Dancing" },
           { id: 3, name: "Paramour" },
-        ]
+        ],
+      githubStatsUrl: "https://github-readme-stats.vercel.app/api?username=zekinah"
     })
 }
 </script>
