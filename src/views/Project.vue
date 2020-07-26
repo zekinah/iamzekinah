e4ab5c988b5de2e6f032a66034d7f0046af6a92f
zekinah <zjlecaros@gmail.com>
2020-07-25 19:10:09 -0700

    Updates in Projects

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,22 +1,104 @@
 <template>
   <div class="page project">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <h1 class="page-header">Projects.</h1>
-    <div class="page-content"></div>
-    <Loading v-show="profileLoading"/>
-    <div class="columns is-multiline">
-        <div v-for="repos of repository" :key="repos.id" class="column is-one-third" v-show="!repos.fork">
-          <Repo :data="repos"/>
+    <div class="featured">
+      <h1 class="page-header">Projects.</h1>
+      <div class="page-content"></div>
+      <div class="featured__project columns is-desktop is-vcentered">
+        <div class="featured__image column">
+          <a href="https://pandemiccovid19.netlify.app/" target="_blank" rel="nofollow noopener noreferrer">
+            <img src="https://raw.githubusercontent.com/zekinah/iamzekinah/master/src/assets/projects/pandemiccovid19.png" alt="pandemiccovid19">
+          </a>
         </div>
+        <div class="featured__details column">
+          <div class="featured__title title is-4 is-spaced">
+            <a href="https://pandemiccovid19.netlify.app/">Pandemic Covid-19</a> 
+          </div>
+          <div class="featured__description subtitle is-6">A Web app and PWA compatible worldwide tracker of Pandemic Covid-19, with visualization of graph, table, and map</div>
+          <div class="featured__builtof subtitle is-6">
+            <div class="tags is-rounded">
+              <span class="tag">VueJS</span>
+              <span class="tag">Bulma</span>
+              <span class="tag">Disease API</span>
+              <span class="tag">Leaflet</span>
+              <span class="tag">Chart</span>
+              <span class="tag">Count Up</span>
+            </div>
+          </div>
+          <div class="featured__link">
+              <div class="links">
+                <a href="">
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                      <path fill="currentColor" d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z" />
+                  </svg>
+                </a>
+              </div>
+              <div class="links">
+                <a href="">
+                  <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                      <path fill="currentColor" d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+        </div>
+      </div>
+      <div class="featured__project columns is-desktop is-vcentered">
+        <div class="featured__details column">
+          <div class="featured__title title is-4 is-spaced">
+            <a href="https://zonepokedex2.netlify.app/">Zone Pokédex 2</a>
+          </div>
+          <div class="featured__description subtitle is-6">A Simple Web App and PWA compatible Pokédex built with VueJS and data was based on <a href="https://pokeapi.co/">Poke API</a>.</div>
+          <div class="featured__builtof subtitle is-6">
+            <div class="tags is-rounded">
+              <span class="tag">VueJS</span>
+              <span class="tag">Vuetify</span>
+              <span class="tag">Poké API</span>
+              <span class="tag">Poké Sprites</span>
+            </div>
+          </div>
+          <div class="featured__link">
+              <div class="links">
+                <a href="">
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                      <path fill="currentColor" d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z" />
+                  </svg>
+                </a>
+              </div>
+              <div class="links">
+                <a href="">
+                  <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                      <path fill="currentColor" d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+        </div>
+        <div class="featured__image column">
+          <a href="https://zonepokedex2.netlify.app/" target="_blank" rel="nofollow noopener noreferrer">
+            <img src="https://raw.githubusercontent.com/zekinah/iamzekinah/master/src/assets/projects/zonepokedex2.png" alt="zonepokedex2">
+          </a>
+        </div>
+      </div>
     </div>
+    <!-- <div class="open-source">
+      <h1 class="page-header">Noteworthy.</h1>
+      <div class="page-content"></div>
+      <Loading v-show="profileLoading"/>
+      <div class="columns is-multiline">
+          <div v-for="repos of repository" :key="repos.id" class="column is-one-third" v-show="!repos.fork">
+            <Repo :data="repos"/>
+          </div>
+      </div>
+    </div> -->
     <SiteFooter/>
   </div>
 </template>
 
 <script>
 import GitHub from "github-api";
-import Loading from "@/components/Loading.vue";
-import Repo from "@/components/Repo.vue";
+// import Loading from "@/components/Loading.vue";
+// import Repo from "@/components/Repo.vue";
 import SiteFooter from "@/components/SiteFooter.vue";
 const github = new GitHub();
 const user = github.getUser('zekinah');
@@ -55,8 +137,8 @@ export default {
     },
   },
   components: {
-    Repo,
-    Loading,
+    // Repo,
+    // Loading,
     SiteFooter
   },
   mounted() {
