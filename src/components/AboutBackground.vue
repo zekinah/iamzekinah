993979abcb7cf7cde0502d02e5f17a909d44ef79
zekinah <zjlecaros@gmail.com>
2021-01-22 12:52:37 +0800

    Updates in Background

--- src/components/AboutBackground.vue
+++ src/components/AboutBackground.vue
@@ -2,9 +2,8 @@
   <div id="__background" class="columns is-desktop section">
       <div class="column section__title is-one-fifth">Background</div>
         <div class="column section__content">
-          <p>I'm currently working as Web Developer at <strong>OptimizeX</strong> building things for web with someone awesome people. Before now, I recently graduated at <strong>Cavite State University</strong> and an interned student in <strong>Amorele Technology</strong>. </p>
-          <p>As a web developer, I always enjoy learning new stuffs that can improve myself in modern technology regarding on web development. My goal is to build web, application and plugins that are scalable and efficient.</p>
-          <p><strong>When I'm not in front of computer</strong>, I usually watched animes and videos on YouTube regarding technology stuff to keep my self motivated.</p>
+          <p>I'm currently working as Web Developer at <strong>OptimizeX Inc.</strong> building things for web with someone awesome people. Before now, I recently graduated at <strong>Cavite State University</strong> and an interned student in <strong>Amorele Technology</strong>. </p>
+          <p>As a web developer, I always enjoy learning new stuffs that can improve myself in modern technology regarding on web development. My goal is to always build great websites and plugins that are scalable and efficient.</p>
         </div>
   </div>
 </template>
