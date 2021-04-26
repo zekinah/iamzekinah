1f1beb6128e2871b56e1e7ec149411b164d00cc9
zekinah <zjlecaros@gmail.com>
2021-04-26 19:37:37 +0800

    Added Fontawesome

--- src/main.js
+++ src/main.js
@@ -1,11 +1,30 @@
 import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import VueAnalytics from 'vue-analytics';
+import App from '@/App.vue'
+import '@/registerServiceWorker'
+import router from '@/router'
+import VueAnalytics from 'vue-analytics'
 import VueMeta from 'vue-meta'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { 
+  faFacebook,
+  faGithub,
+  faLinkedin,
+  faWordpress
+} from '@fortawesome/free-brands-svg-icons'
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 
 Vue.use(VueMeta)
+
+library.add(
+  faFacebook,
+  faGithub,
+  faLinkedin,
+  faWordpress
+)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
