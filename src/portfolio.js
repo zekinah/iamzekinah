23111b4bcdaa493a953d41360d34f11370c439dc
zekinah <zjlecaros@gmail.com>
2021-04-27 16:10:51 +0800

    init portfolio --Added introduction

---
+++ src/portfolio.js
@@ -0,0 +1,33 @@
+/**
+ * Porfolio details
+ * version v1.0.2
+ */
+
+const introduction = {
+    name: "Zekinah Lecaros",
+    tagline: "A person who will strive for his achievements with the use of digital tools and open source.",
+    email: "zjlecaros@gmail.com",
+    resume_link: "//drive.google.com/file/d/1NovnG3rvqO2wjGranm7TvfigIeAd0W8D/view",
+    follow_me: [
+        {
+            social: [
+                {
+                    social_link: "http://facebook.com/zekinahhh",
+                    social_icon: "facebook"
+                },
+                {
+                    social_link: "https://github.com/zekinah",
+                    social_icon: "github"
+                },
+                {
+                    social_link: "https://ph.linkedin.com/in/zekinah",
+                    social_icon: "linkedin"
+                },
+                {
+                    social_link: "https://profiles.wordpress.org/zekinah/",
+                    social_icon: "wordpress"
+                },
+            ]
+        }
+    ]
+ };
