a9358d105afd6e654e922d4df10ff5b64c97645b
zekinah <zjlecaros@gmail.com>
2021-04-27 17:42:08 +0800

    Updated portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,27 +11,23 @@ const introduction = {
     tagline: "A person who will strive for his achievements with the use of digital tools and open source.",
     email: "zjlecaros@gmail.com",
     resume_link: "//drive.google.com/file/d/1NovnG3rvqO2wjGranm7TvfigIeAd0W8D/view",
-    follow_me: [
+    social: [
         {
-            social: [
-                {
-                    social_link: "http://facebook.com/zekinahhh",
-                    social_icon: "facebook"
-                },
-                {
-                    social_link: "https://github.com/zekinah",
-                    social_icon: "github"
-                },
-                {
-                    social_link: "https://ph.linkedin.com/in/zekinah",
-                    social_icon: "linkedin"
-                },
-                {
-                    social_link: "https://profiles.wordpress.org/zekinah/",
-                    social_icon: "wordpress"
-                },
-            ]
-        }
+            social_link: "http://facebook.com/zekinahhh",
+            social_icon: "facebook"
+        },
+        {
+            social_link: "https://github.com/zekinah",
+            social_icon: "github"
+        },
+        {
+            social_link: "https://ph.linkedin.com/in/zekinah",
+            social_icon: "linkedin"
+        },
+        {
+            social_link: "https://profiles.wordpress.org/zekinah/",
+            social_icon: "wordpress"
+        },
     ]
  };
 
