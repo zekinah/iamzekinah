111964e1c15ed7b5643ef10f8aaedd3d0b9bdf35
zekinah <zjlecaros@gmail.com>
2021-04-30 19:01:10 +0800

    Updated homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="home">
     <header class="intro">
-      <h1 class="intro__hello">> Hi, I'm <span class="name">Zekinah Lecaros. <span class="blink">_</span></span></h1>
-      <h2 class="intro__tagline">A person who will strive for his achievements with the use of digital tools and open source.</h2>
+      <h1 class="intro__hello">> Hi, I'm <span class="name">{{intro.name}}. <span class="blink">_</span></span></h1>
+      <h2 class="intro__tagline">{{intro.tagline}}</h2>
       <h3 class="intro__contact">
         <span>Get in touch </span>
-        <span> <a href="mailto:zjlecaros@gmail.com" target="_blank" class="highlight-link">zjlecaros@gmail.com</a> </span>
+        <span> <a :href="`mailto:${intro.email}`" target="_blank" class="highlight-link">{{intro.email}}</a> </span>
       </h3>
     </header>
     <followme/>
   </div>
 </template>
 <script>
+import {introduction} from "@/portfolio";
 import followme from '@/components/Followme.vue'
-
 export default {
   name: 'home',
+  data: () => ({
+    intro: introduction
+  }),
   components: {
     followme
   },
