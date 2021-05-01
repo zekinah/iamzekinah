4f6a22ffe223126b032a8ac9911757586add22db
zekinah <zjlecaros@gmail.com>
2021-05-01 09:02:35 +0800

    Updates Skills component

--- src/components/AboutSkills.vue
+++ src/components/AboutSkills.vue
@@ -3,11 +3,11 @@
     <div class="column section__title is-one-fifth">Skills</div>
     <div class="column section__content">
       <div class="skills">
-        <div class="skills__content" v-for="skill of skills" :key="skill.skill_id">
-          <div class="skills__title">{{skill.skill_title}}</div>
+        <div class="skills__content" v-for="(s, index) in sSet.skills" :key="index">
+          <div class="skills__title">{{s.skill_title}}</div>
           <div class="skills__list">
             <ul>
-              <li v-for="s of skill.skill_list" :key="s.skill_name">{{s.skill_name}}</li>
+              <li v-for="s of s.skill_list" :key="s.skill_name">{{s.skill_name}}</li>
             </ul>
           </div>
         </div>
@@ -17,35 +17,12 @@
 </template>
 
 <script>
+import { skillSet } from "@/portfolio";
+
 export default {
     name: 'AboutSkills',
      data: () => ({
-      skills: [
-        {
-          skill_id: 1,
-          skill_title: "Technologies",
-          skill_list: [
-            { skill_name: "Javascript (ES6)", },
-            { skill_name: "HTML5 & (S)CSS", },
-            { skill_name: "jQuery + Ajax", },
-            { skill_name: "VueJS + Axios", },
-            { skill_name: "My Sqli", },
-            { skill_name: "PHP", } 
-          ]
-        },
-        {
-          skill_id: 2,
-          skill_title: "CMS and Tools",
-          skill_list: [
-            { skill_name: "Wordpress & Woocommerce", },
-            { skill_name: "Magento 1.9 & 2", },
-            { skill_name: "GIT & SVN", },
-            { skill_name: "Bootstrap", },
-            { skill_name: "Vuetify", },
-            { skill_name: "Bulma", }
-          ]
-        }
-      ]
+      sSet: skillSet
     })
 }
 </script>
